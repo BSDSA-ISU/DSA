974ea66d04b456bc7439dd723288fb07e6d4b2f4
AlieeLinux <bazartroy531@gmail.com>
2025-08-29 21:31:59 +0800

    remove test

--- Test.java
+++
@@ -1,9 +0,0 @@
-import AllInOneAlgo.Algos;
-
-public class Test {
-    public static void main(String[] args) {
-        Algos alg = new Algos();
-        alg.stop = 99;
-        System.out.println(alg.GreatestCommonDivisor(48, 18));
-    }
-}
